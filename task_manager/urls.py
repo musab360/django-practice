76ad1db3eb2dc6546957a022ec9f6d82db4d5b64
musab360 <musab.ahmed@bluecascade.org>
2026-05-08 12:50:53 +0500

    full project made

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create-task', views.task_creation, name='create_task'),
-    path('retrieve-tasks', views.task_retrieval, name='retrieve_tasks'),
+    path('retrieve-tasks/', views.task_retrieval, name='retrieve_tasks'),
+    path('update-task', views.task_update, name='update_task'),
+    path('retrieve-updates/', views.task_retrieve_updates, name='retrieve_updates'),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -11,12 +11,12 @@ from .models import TaskCreation, TaskUpdated
 def task_creation(request):
     user = request.jwt_user  # Access decoded JWT payload
     request_data = json.loads(request.body)
-    TaskCreation.objects.create(
+    task = TaskCreation.objects.create(
         created_by_id=user.get("user_id"),
         title=request_data.get("title"),
         description=request_data.get("description")
     )
-    return JsonResponse({"response": "Task created successfully!"})
+    return JsonResponse({"response": "Task created successfully!", "id": task.id})
 
 @csrf_exempt
 @require_jwt
@@ -36,3 +36,46 @@ def task_retrieval(request):
         for task in tasks
     ]
     return JsonResponse({"tasks": tasks_list})
+
+@csrf_exempt
+@require_jwt
+def task_update(request):
+    user = request.jwt_user  # Access decoded JWT payload
+    if request.method != 'POST':
+        return JsonResponse({"error": "Use POST method"}, status=400)
+    request_data = json.loads(request.body)
+    task_id = request_data.get("task_id")
+    description = request_data.get("description")
+
+    updated_task = TaskUpdated.objects.create(
+        task_id=task_id,
+        updated_by_id=user.get("user_id"),
+        update_description=description
+    )
+
+    return JsonResponse({
+        "response": f"Task {task_id} updated successfully with {updated_task.update_description} having ID {updated_task.id}",
+        "id": task_id,
+        })
+
+@csrf_exempt
+@require_jwt
+def task_retrieve_updates(request):
+    user = request.jwt_user  # Access decoded JWT payload
+    task_id = request.GET.get("task_id")
+
+    updates = TaskUpdated.objects.filter(
+        task_id=task_id,
+        updated_by_id=user.get("user_id")
+    )
+
+    updates_list = [
+        {
+            "id": update.id,
+            "update_description": update.update_description,
+            "updated_at": update.updated_at
+        }
+        for update in updates
+    ]
+
+    return JsonResponse({"updates": updates_list, "total_updates": len(updates_list)})
